7b9d519ae17be62ccf6d5be44cbe9cb72f4a4eaf
Dsartok <sery2vld@gmail.com>
2020-09-17 20:14:21 +0300

    created configuration for svetle app (#1)
    * created configuration for svetle app
    
    * removed yarn-error
    
    Co-authored-by: Vladimir Sery <v.seriy@isoit.ru>

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from "~/pages/App.svelte";
+
+new App({
+  target: document.getElementById("svelte_app"),
+});
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".mjs", ".js", ".svelte"],
+    alias: {
+      "~": path.resolve(__dirname, "src/"),
+    },
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./public"),
+    publicPath: "/build",
+    compress: true,
+    port: 3020,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: "svelte-loader",
+          options: {
+            emitCss: true,
+            hotReload: true,
+          },
+        },
+      },
+    ],
+  },
+  mode: "development",
+};
